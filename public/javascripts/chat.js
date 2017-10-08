54df26d945a2735818dc016a12b447db940a4a40
nagarjun28 <nagarjun28@github.com>
2017-10-08 07:51:02 +0000

    afterfrontend

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,24 +2,19 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
 
-$scope.messages=[
-    {'sender':'USER',
-      'text':'hi',
-      'time':'10:01AM'
-    },
-    {'sender':'BOT',
-      'text':'hi how can i help you',
-      'time':'10:01AM'
-    },
-    {'sender':'USER',
-      'text':'give me news about north korea',
-      'time':'10:01AM'
-    },
-    {'sender':'BOT',
-      'text':'invalid',
-      'time':'10:01AM'
-    }
-
-];
+$scope.messages=[];
+var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+    exampleSocket.onmessage  =   function  (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date().toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+    $scope.sendMessage = function () {
+            exampleSocket.send($scope.userMessage);
+            $scope.userMessage = "";
+        };
+        $scope.trust=$sce.trustAsHtml;
 
 });
