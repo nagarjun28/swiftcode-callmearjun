22d883e1ba585b0344cb36cde925a5243b1f41a9
nagarjun28 <nagarjun28@github.com>
2017-10-08 04:44:00 +0000

    afternewbg

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,25 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+
+$scope.messages=[
+    {'sender':'USER',
+      'text':'hi',
+      'time':'10:01AM'
+    },
+    {'sender':'BOT',
+      'text':'hi how can i help you',
+      'time':'10:01AM'
+    },
+    {'sender':'USER',
+      'text':'give me news about north korea',
+      'time':'10:01AM'
+    },
+    {'sender':'BOT',
+      'text':'invalid',
+      'time':'10:01AM'
+    }
+
+];
+
 });
